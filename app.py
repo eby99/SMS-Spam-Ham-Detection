e8bfd2f8508204c1faddc48d72552b8c5b41b3a1
eby99 <ebymathew619@gmail.com>
2024-10-21 21:55:31 +0530

    Updated

--- app.py
+++ app.py
@@ -1,57 +1,42 @@
 import streamlit as st
-import sklearn
 import pickle
-import string
 import re
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-nltk.download('stopwords')
-nltk.download('punkt')
+from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+nltk.download('stopwords')
 
 port_stemmer = PorterStemmer()
 
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Create a function to generate cleaned data from raw text
-
 def clean_text(text):
-    text = word_tokenize(text) # Create tokens
-    text= " ".join(text) # Join tokens
-    text = [char for char in text if char not in string.punctuation] # Remove punctuations
-    text = ''.join(text) # Join the leters
-    text = [char for char in text if char not in re.findall(r"[0-9]", text)] # Remove Numbers
-    text = ''.join(text) # Join the leters
-    text = [word.lower() for word in text.split() if word.lower() not in set(stopwords.words('english'))] # Remove common english words (I, you, we,...)
-    text = ' '.join(text) # Join the leters
-    text = list(map(lambda x: port_stemmer.stem(x), text.split()))
-    return " ".join(text)   # error word
-
+    text = re.sub(r'[^\w\s]', '', text)
+    text = re.sub(r'\d', '', text)
+    tokens = word_tokenize(text)
+    tokens = [word.lower() for word in tokens if word.lower() not in set(stopwords.words('english'))]
+    tokens = list(map(lambda x: port_stemmer.stem(x), tokens))
+    return " ".join(tokens)
 
 st.title('SMS Spam Classifier')
 
 input_sms = st.text_input("Enter the Message")
 
 if st.button('Predict'):
-
     if input_sms == "":
         st.header('Please Enter Your Message !!!')
-
     else:
-
-        # 1. Preprocess
         transform_text = clean_text(input_sms)
-
-        # 2. Vectorize
         vector_input = tfidf.transform([transform_text])
-
-        # 3. Prediction
         result = model.predict(vector_input)
-
-        # 4. Display
-
         if result == 1:
             st.header("Spam")
         else:
