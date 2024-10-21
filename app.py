1a71d4fc4cdd7e04e8cb5c3f6fc8b8d5d2e9228d
eby99 <ebymathew619@gmail.com>
2024-10-21 22:21:27 +0530

    Updated

--- app.py
+++ app.py
@@ -6,37 +6,59 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
+# Ensure necessary NLTK resources are downloaded
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
     nltk.download('punkt')
 
-nltk.download('stopwords')
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
 
+# Initialize the Porter Stemmer
 port_stemmer = PorterStemmer()
 
+# Load the pre-trained model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
 def clean_text(text):
+    # Remove punctuation and numbers
     text = re.sub(r'[^\w\s]', '', text)
     text = re.sub(r'\d', '', text)
+    
+    # Tokenize the text
     tokens = word_tokenize(text)
+    
+    # Remove stopwords and stem the tokens
     tokens = [word.lower() for word in tokens if word.lower() not in set(stopwords.words('english'))]
     tokens = list(map(lambda x: port_stemmer.stem(x), tokens))
+    
+    # Join the tokens back together
     return " ".join(tokens)
 
+# Streamlit app title
 st.title('SMS Spam Classifier')
 
+# User input for SMS message
 input_sms = st.text_input("Enter the Message")
 
 if st.button('Predict'):
     if input_sms == "":
         st.header('Please Enter Your Message !!!')
     else:
+        # Preprocess the input message
         transform_text = clean_text(input_sms)
+        
+        # Vectorize the cleaned text
         vector_input = tfidf.transform([transform_text])
+        
+        # Make a prediction
         result = model.predict(vector_input)
+        
+        # Display the result
         if result == 1:
             st.header("Spam")
         else:
